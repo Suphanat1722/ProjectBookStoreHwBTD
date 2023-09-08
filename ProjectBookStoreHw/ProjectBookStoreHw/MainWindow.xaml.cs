8d556e3179ca11e4beab7805561282546ee1f1ca
Suphanat1722 <suphanat1722@gmail.com>
2023-09-08 13:26:58 +0700

    รวม code จากไฟล์อื่น และแก้ไขการส่ง Mail
    - นำ code จากไฟล์ SentEmai.py มารวม
    - แก้ไขการส่งข้อความใน Mail  เป็นรูปแบบ
      HTML เพื่อความสวยงาม
    - เปลี่ยนเงื่อนไงเป็น เมื่อ dataSoon
      เท่ากับ 7 ให้ส่งEmail

--- ProjectBookStoreHw/ProjectBookStoreHw/MainWindow.xaml.cs
+++ ProjectBookStoreHw/ProjectBookStoreHw/MainWindow.xaml.cs
@@ -28,5 +28,17 @@ namespace ProjectBookStoreHw
             BookWindow bookWindow = new BookWindow();
             bookWindow.ShowDialog();
         }
+
+        private void ButtonToCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            CustomerWindow customerWindow = new CustomerWindow();
+            customerWindow.ShowDialog();
+        }
+
+        private void ButtonToTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            TransactionWindow transactionWindow = new TransactionWindow();
+            transactionWindow.ShowDialog();
+        }
     }
 }
